0cbf49b6e822abd9f86b194dca79d943f2c92592
Nhan Nguyen <nathan13574@gmail.com>
2020-05-08 02:52:02 -0500

    updated cat data assignment

---
+++ express/cat_data/static/js/test.js
@@ -0,0 +1,13 @@
+cat = [{
+        fav_food: 'Spaghetti'
+    },
+    {
+        age: 3
+    }, {
+        sleeping_spots: 'under the bed in a sunbeam'
+    }
+]
+
+for (var i in cat) {
+    console.log(cat[i])
+}
