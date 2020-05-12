fd19866744ea1a40a3b55ca13a6d914f030c5314
Nathan Nguyen <nhan13574@gmail.com>
2020-05-11 22:18:20 -0500

    updated

--- mongoDB/quotingDojo/server.js
+++ mongoDB/quotingDojo/server.js
@@ -1,77 +1,75 @@
 // Import
-const express = require('express')
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+var moment = require("moment");
 
-mongoose.connect('mongodb://localhost/name_of_your_DB', {
-    useNewUrlParser: true
+mongoose.connect("mongodb://localhost/name_of_your_DB", {
+  useNewUrlParser: true,
 });
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String
-    },
-    quote: {
-        type: String
-    },
-    created_at: {
-        type: Date,
-        default: Date.now()
-    }
-})
+  name: {
+    type: String,
+  },
+  quote: {
+    type: String,
+  },
+  created_at: {
+    type: Date,
+    default: moment().format("llll"),
+  },
+});
 // Create an object that contains methods for mongoose to interface with MongoDB
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
 // Setting
-app.use(express.static(__dirname + '/static'))
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
+app.use(express.static(__dirname + "/static"));
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}))
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // Routes
-app.get('/', (req, res) => {
-    res.render('index')
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-
-
-app.post('/process', (req, res) => {
-    const user = new User();
-    user.name = req.body.name
-    user.quote = req.body.quote
-    user.save()
-        .catch(err => {
-            console.log('Error saving user:', err)
-        })
-    res.redirect('/quotes')
-})
-
-
-app.get('/quotes', (req, res) => {
-    User.find({}, (err, quotes) => {
-        var context = {
-            quotes: quotes
-        };
-
-        if (err) {
-            console.log('Something went wrong')
-        }
-
-        res.render("quotes", context);
-    });
+app.post("/process", (req, res) => {
+  const user = new User();
+  user.name = req.body.name;
+  user.quote = req.body.quote;
+  user.save().catch((err) => {
+    console.log("Error saving user:", err);
+  });
+  res.redirect("/quotes");
 });
 
-app.get('/delete', (req, res) => {
-    User.remove({})
-        .then(data => {
-            res.json(data)
-        })
-    res.redirect('/quotes');
-})
+app.get("/quotes", (req, res) => {
+  User.find({}, (err, quotes) => {
+    var context = {
+      quotes: quotes,
+    };
 
-app.listen(8000)
+    if (err) {
+      console.log("Something went wrong");
+    }
+
+    res.render("quotes", context);
+  });
+});
+
+app.get("/delete", (req, res) => {
+  User.remove({}).then((data) => {
+    res.json(data);
+  });
+  res.redirect("/quotes");
+});
+
+app.listen(8000);
