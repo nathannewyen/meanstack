2075e6bfe790af1b1402bb7fcc91a0bc5671553d
Nhan Nguyen <nathan13574@gmail.com>
2020-05-14 17:15:30 -0500

    upload restfulAPI assignment

---
+++ mongoDB/restfulAPI/server/config/route.js
@@ -0,0 +1,39 @@
+const users = require('../controllers/users.js')
+
+module.exports = function (app) {
+    app.get("/", (req, res) => {
+        users.reg_login(req, res)
+    });
+
+    app.get("/tasks", (req, res) => {
+        users.show(req, res)
+    })
+
+    app.post("/tasks", (req, res) => {
+        users.add_user(req, res)
+    })
+
+    app.get("/show", (req, res) => {
+        users.show(req, res)
+    })
+
+    app.post("/reg_process", (req, res) => {
+        users.reg_process(req, res)
+    })
+
+    app.post('/login_process', (req, res) => {
+        users.login_process(req, res)
+    })
+
+    app.get("/dashboard", (req, res) => {
+        users.dashboard(req, res)
+    })
+
+    app.get('/logout', (req, res) => {
+        users.logout(req, res)
+    })
+
+    app.get("/user/:id", (req, res) => {
+        users.user_info(req, res)
+    })
+}
